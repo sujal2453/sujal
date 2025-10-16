773ff1c19419542a58c524e0b70b518c62b96747
sujal2453 <sujalwagholikar25@gmail.com>
2025-10-16 17:47:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "sujal-donation-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "crypto": "^1.0.1",
+    "cors": "^2.8.5",
+    "qrcode": "^1.5.0"
+  }
+}
